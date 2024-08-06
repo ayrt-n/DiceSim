2330f907926fc7e017285ee0e24a80377b4aa4d6
ayrton <ayrton.parkinson1@gmail.com>
2024-08-06 08:35:07 -0400

    Initial Commit

---
+++ Day95Challenge/Day95ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day95ChallengeApp.swift
+//  Day95Challenge
+//
+//  Created by Ayrton Parkinson on 2024/08/06.
+//
+
+import SwiftUI
+
+@main
+struct Day95ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
