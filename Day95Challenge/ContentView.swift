1d8c7ab76b46dd363cc0c1b1751adac9df618e6a
ayrton <ayrton.parkinson1@gmail.com>
2024-08-09 07:37:53 -0400

    Change main content view to use tabview

--- Day95Challenge/ContentView.swift
+++ Day95Challenge/ContentView.swift
@@ -9,68 +9,18 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) var modelContext
-    @State private var dice: Dice
-    @State private var rollingDice = false
-    @State private var diceTime = 10
-    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-    
     var body: some View {
-        NavigationStack {
-            VStack {
-                HStack {
-                    ForEach(dice.diceArray) { die in
-                        DiceView(die: die)
-                    }
+        TabView {
+            DiceRollView()
+                .tabItem {
+                    Label("Roll Dice", systemImage: "dice")
                 }
-                
-                HStack {
-                    Text("Roll Total: ")
-                    Text("\(dice.total)")
-                        .frame(width: 30)
+            
+            DiceHistoryView()
+                .tabItem {
+                    Label("Roll History", systemImage: "arrow.counterclockwise")
                 }
-                .font(.headline)
-                .padding(.vertical)
-
-                Button(action: rollDice) {
-                    Text("Roll Dice")
-                        .frame(width: 150)
-                }
-                .buttonStyle(.borderedProminent)
-                .disabled(rollingDice)
-                .sensoryFeedback(.selection, trigger: rollingDice)
-            }
-            .toolbar {
-                NavigationLink() {
-                    EditDiceView(dice: dice)
-                } label: {
-                    Label("Edit", systemImage: "gearshape")
-                }
-            }
-            .onReceive(timer) { _ in
-                guard rollingDice else { return }
-                
-                dice.rollDice()
-                diceTime -= 1
-                
-                if diceTime <= 0 {
-                    resetDiceRoll()
-                }
-            }
         }
-    }
-
-    init() {
-        _dice = State(initialValue: Dice(diceArray: [Die(sides: 6), Die(sides: 20), Die(sides: 10)]))
-    }
-    
-    func rollDice() {
-        rollingDice = true
-    }
-    
-    func resetDiceRoll() {
-        rollingDice = false
-        diceTime = 10
     }
 }
 
